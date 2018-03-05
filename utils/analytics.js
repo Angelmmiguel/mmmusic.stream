19082d398e33ac6ed53f5db7465d84a399df5ee2
Angel M De Miguel <angel@bitnami.com>
2018-03-05 14:37:51 +0100

    Remove logging

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,12 +1,10 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  console.log('GA init');
   ReactGA.initialize('UA-47913999-13');
 }
 
 export const logPageView = () => {
-  console.log(`Logging pageview for ${window.location.pathname}`);
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
 }
