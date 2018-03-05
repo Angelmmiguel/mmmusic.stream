90f6df195547316aa3a18e491ccaa46ba59dfe59
Angel M <angel@laux.es>
2018-03-06 00:25:05 +0100

    Update the footer. This closes #2

--- components/Footer.js
+++ components/Footer.js
@@ -7,12 +7,21 @@ class Footer extends React.PureComponent {
       <section>
         <div className="grid">
           <div className="cell">
-            © mmmusic { year }. Develop by
+            © mmmusic { year }. An
             { ' ' }
-            <a href="https://twitter.com/laux_es" target="_blank" rel="noopener">Angel</a>.
-            If you like this, you can buy me a
+            <a href="https://github.com/Angelmmiguel/mmmusic.stream" target="_blank" rel="noopener">Open Source project</a>
             { ' ' }
-            <a href="https://ko-fi.com/angelmmiguel" target="_blank" rel="noopener">ko-fi</a>
+            created with
+            { ' ' }
+            <a href="https://ko-fi.com/angelmmiguel" target="_blank" rel="noopener">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+              </svg>
+            </a>
+            { ' ' }
+            by
+            { ' ' }
+            <a href="https://twitter.com/_angelmm" target="_blank" rel="noopener">Angel</a>
           </div>
           <div className="cell tr">
             mmmusic is not affiliated with
@@ -36,6 +45,12 @@ class Footer extends React.PureComponent {
         a {
           color: var(--c-red);
           text-decoration: none;
+        }
+
+        svg {
+          position: relative;
+          top: -1px;
+          vertical-align: middle;
         }
       `}</style>
     </footer>;
