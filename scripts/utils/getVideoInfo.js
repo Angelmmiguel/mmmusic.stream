6f3a64d4a5b8f78b72ba2fdd6eedf869f9360c9f
Angel M <angel@laux.es>
2019-10-05 00:45:22 +0200

    Fix script to get the Youtube info. Delete non existent channels

--- scripts/utils/getVideoInfo.js
+++ scripts/utils/getVideoInfo.js
@@ -3,8 +3,18 @@ const fetch = require('isomorphic-unfetch');
 // Get the info for the given video
 const getVideoInfo = async (id, token) => {
   const res = await fetch(
-    `https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${token}%20&part=snippet,status&fields=items(snippet(title,liveBroadcastContent,tags),status(uploadStatus,embeddable,privacyStatus))`);
+    `https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${token}&part=snippet,status`);
   const json = await res.json();
+
+  if (res.status !== 200) {
+    console.error(`[https://youtube.com/watch?v=${id}] Error retrieving the video info: `);
+
+    if (json.error != null && json.error.message != null) {
+      console.error(`[https://youtube.com/watch?v=${id}] ${json.error.message}`);
+    } else {
+      console.error(json);
+    }
+  }
   return res.status === 200 ? json.items[0] : null;
 }
 
